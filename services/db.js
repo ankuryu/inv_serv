77eec1e3963ae2c3afc2a9c7f0fb7887a7c43ead
Ankuryu <pmco89@gmail.com>
2022-05-01 19:47:51 +0530

    checking with db3

--- services/db.js
+++ services/db.js
@@ -1,7 +1,7 @@
-const sqlite = require('sqlite3');
+const sqlite = require('sqlite3').verbose();
 const path = require("path");
-console.log("Directory : ",path.resolve(__curdir,'db'))
-const db = new sqlite(path.resolve('inv.db3'));
+console.log("Directory : ",path.resolve(__dirname,'../db/inv2122.db3'))
+const db = new sqlite.Database(path.resolve('../db/inv2122.db3'));
 
 function query(sql,params){
  return db.prepare(ql).all(params);
--- services/inv.js
+++ services/inv.js
@@ -1,6 +1,6 @@
 const db = require('./db.js')
 const config = require('../config')
-const db = require('../services/db')
+const db3 = require('../services/db')
 
 function getMultiple(page=1){
   
@@ -9,23 +9,31 @@ function getMultiple(page=1){
 
 function getAllMfg(){
   let sql = "Select distinct mfg from pmisu ;"
-  ret = db.query(sql,[]); 
+  ret = db3.query(sql,[]); 
   return ret 
 
 }
 
 function getAllIcode() {
   let sql = "Select distinct icode from pmisu ;"
-  ret = db.query(sql,[]); 
+  ret = db3.query(sql,[]); 
   return ret 
 
 }
 
 function getIsu(mfg,icode){
+  let sql = "Select mfg,icode,asize,sum(qty) from pmisu where mfg = ? and icode = ? group by mfg,icode,asize;"
+  ret = db3.query(sql,[]); 
+  return ret 
 
 }
 
 function getRcp(mfg,icode){
+
+  let sql = "Select mfg,icode,asize,sum(qty) from pmrcp where mfg = ? and icode = ? group by mfg,icode,asize;"
+  ret = db3.query(sql,[]); 
+  return ret 
+
 
 
 }
