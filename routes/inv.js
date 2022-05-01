8a665b54799050c605943e9843c27d03043718cb
Ankuryu <pmco89@gmail.com>
2022-05-01 19:06:56 +0530

    Included connection between routes/inv and services/inv

--- routes/inv.js
+++ routes/inv.js
@@ -1,41 +1,61 @@
 const express = require('express')
 const router = express.Router();
-const inv = require('../routes/inv.js');
+const inv = require('../services/inv.js');
 
-router.get('/api/mfg/:mfg', (req,res,next)=>{
-  let mfg = req.params.mfg
-  msg = `The mfg is ${mfg}`
-  res.send(msg).statusCode(200)
+router.get('/api/mfg/', (req,res,next)=>{
+  try {
+    let rslt = inv.getAllMfg()
+    res.send(msg).statusCode(200)
+  } catch(err){
+    console.log(err.message)
+  }
 });
 
-router.get('/api/icode/:icode',(req,res,next)=>{
-  let icode = req.params.icode
-  msg = `The icode is ${icode}`
-  res.send(msg).statusCode(200)
+router.get('/api/icode/',(req,res,next)=>{
+
+  try {
+    let rslt = inv.getAllIcode()
+    msg = `The icode is `
+    res.send(msg).statusCode(200)
+  } catch(err){
+    console.log(err.message)
+  }
 })
 
 
-router.get('/api/asize/:asize',(req,res,next)=>{
-  let asize = req.params.asize
-  msg = `The asize is ${asize}`
-  res.send(msg).statusCode(200)
+router.get('/api/asize/',(req,res,next)=>{
+  try {
+    let rslt = inv.getAllMfg()
+    msg = `The asize is `
+    res.send(msg).statusCode(200)
+  } catch(err){
+    console.log(err.message)
+  }
 })
 
 
-router.get('/api/isu/:mfg/:icode/:asize',(req,res,next)=>{
-  let mfg = req.params.mfg
-  let icode = req.params.icode
-  let asize = req.params.asize
-  msg = `The Sales of mfg is ${mfg} icode is ${icode} and asize  is ${asize}`
-  res.send(msg).statusCode(200)
+router.get('/api/isu/:mfg/:icode/',(req,res,next)=>{
+  try {
+    let mfg = req.params.mfg
+    let icode = req.params.icode
+    let rslt = inv.getIsu()
+    msg = `The Sales of mfg is ${mfg} icode is ${icode} and asize  is ${asize}`
+    res.send(msg).statusCode(200)
+  } catch(err){
+    console.log(err.message)
+  }
 })
 
 router.get('/api/rcp/:mfg/:icode/:asize',(req,res,next)=>{
-  let mfg = req.params.mfg
-  let icode = req.params.icode
-  let asize = req.params.asize
-  msg = `The Receipt of mfg is ${mfg} icode is ${icode} and asize  is ${asize}`
-  res.send(msg).statusCode(200)
+  try {
+    let mfg = req.params.mfg;
+    let icode = req.params.icode;
+    let rslt = inv.getRcp();
+    msg = `The Receipt of mfg is ${mfg} icode is ${icode} and  `
+    res.send(msg).statusCode(200)
+  } catch(err){
+    console.log(err.message)
+  }
 
 })
 
--- services/inv.js
+++ services/inv.js
@@ -1,12 +1,41 @@
 const db = require('./db.js')
 const config = require('../config')
+const db = require('../services/db')
 
 function getMultiple(page=1){
+  
+
+}
+
+function getAllMfg(){
+  let sql = "Select distinct mfg from pmisu ;"
+  ret = db.query(sql,[]); 
+  return ret 
+
+}
+
+function getAllIcode() {
+  let sql = "Select distinct icode from pmisu ;"
+  ret = db.query(sql,[]); 
+  return ret 
+
+}
+
+function getIsu(mfg,icode){
+
+}
+
+function getRcp(mfg,icode){
+
 
 }
 
 
 module.exports = {
   getMultiple,
+  getAllMfg,
+  getAllIcode,
+  getIsu,
+  getRcp,
   
 }
